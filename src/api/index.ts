9374c9ba5aec99ec0892ac5bd17bec9ecb97d389
ioseb <soso.bichinashvili@gmail.com>
2022-07-13 14:43:59 +0400

    fix: builds and runs

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,9 @@ import { RemoteProvider } from './remote-provider';
 
 export { getRandomFact } from './facts';
 
+console.log(process.env.REACT_APP_API);
+console.log(process.env.REACT_APP_FACTS_URL);
+console.log(process.env.REACT_APP_REMOTE_API);
 export const api: IProvider =
   process.env.REACT_APP_API === 'remote'
     ? new RemoteProvider()
